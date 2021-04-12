bff14d8d67e10eec053831ba652012ea25b391bd
Daniel Fontoura <dfontoura@yahoo.com.br>
2021-04-12 11:38:18 -0300

    feat: expose route get /purchase-orders

--- src/main/routes/purchase-order.js
+++ src/main/routes/purchase-order.js
@@ -1,6 +1,8 @@
 const { adaptRoute } = require('../adapters/express-router-adapter');
+const makeFindAllPurchaseOrdersController = require('../factories/controllers/purchase-orders/find-all-purchase-orders');
 const makeCreatePurchaseOrderController = require('../factories/controllers/purchase-orders/create-purchase-orders');
 
 module.exports = (router) => {
+    router.get('/purchase-orders', adaptRoute(makeFindAllPurchaseOrdersController()));
     router.post('/purchase-orders', adaptRoute(makeCreatePurchaseOrderController()));
 };
